729039caf58acdf083847073813bf7635f7956ba
Tulpa <openclaw@tulpa>
2026-01-30 11:56:26 +0000

    Fix: Fallback file size via Content-Length when missing

--- FileStream/utils/render_template.py
+++ FileStream/utils/render_template.py
@@ -10,7 +10,7 @@ db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
 async def render_page(db_id):
     file_data=await db.get_file(db_id)
     src = urllib.parse.urljoin(Server.URL, f'dl/{file_data["_id"]}')
-    file_size = humanbytes(file_data['file_size'])
+    file_size = humanbytes(file_data.get('file_size') or 0)
     file_name = file_data['file_name'].replace("_", " ")
 
     base_dir = os.path.dirname(os.path.dirname(__file__))  # FileStream/
@@ -19,6 +19,9 @@ async def render_page(db_id):
         template_file = os.path.join(base_dir, "template", "play.html")
     else:
         template_file = os.path.join(base_dir, "template", "dl.html")
+
+    # Fallback to Content-Length when size is missing/zero
+    if not file_data.get('file_size'):
         async with aiohttp.ClientSession() as s:
             async with s.get(src) as u:
                 length = u.headers.get('Content-Length')
