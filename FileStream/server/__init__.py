5558675847e593f97ca9c0c8afd17bb3a9a7c87e
Tulpa <openclaw@tulpa>
2026-01-30 08:14:24 +0000

    Fix: Use absolute path for static files serving

--- FileStream/server/__init__.py
+++ FileStream/server/__init__.py
@@ -7,8 +7,12 @@ def web_server():
     web_app.add_routes(routes)
 
     # -----------------[ STATIC FILES SETUP ]----------------- #
-    # This serves files from 'FileStream/static/' at the URL '/static/'
-    static_path = 'FileStream/static'
+    # Resolve static folder path dynamically (FileStream/static)
+    # This ensures it works regardless of CWD (Docker, local, etc.)
+    current_dir = os.path.dirname(os.path.abspath(__file__))  # FileStream/server
+    package_dir = os.path.dirname(current_dir)                # FileStream
+    static_path = os.path.join(package_dir, 'static')
+    
     if os.path.isdir(static_path):
         web_app.router.add_static('/static/', path=static_path, name='static')
     else:
