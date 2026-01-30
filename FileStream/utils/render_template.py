8f9df411fd0cd98fde49b0a6dae5ad07651977cd
Tulpa <openclaw@tulpa>
2026-01-30 12:51:42 +0000

    Fix: Guard missing file_name in render_page

--- FileStream/utils/render_template.py
+++ FileStream/utils/render_template.py
@@ -11,7 +11,7 @@ async def render_page(db_id):
     file_data=await db.get_file(db_id)
     src = urllib.parse.urljoin(Server.URL, f'dl/{file_data["_id"]}')
     file_size = humanbytes(file_data.get('file_size') or 0)
-    file_name = file_data['file_name'].replace("_", " ")
+    file_name = (file_data.get('file_name') or 'file').replace("_", " ")
 
     base_dir = os.path.dirname(os.path.dirname(__file__))  # FileStream/
     mime_type = (file_data.get('mime_type') or '').split('/')[0].strip()
