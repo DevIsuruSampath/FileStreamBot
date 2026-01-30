bd75974a932551a2ac2a90e95bbdb61953f88659
Tulpa <openclaw@tulpa>
2026-01-30 07:56:16 +0000

    Finalize template UI updates

--- FileStream/server/__init__.py
+++ FileStream/server/__init__.py
@@ -1,7 +1,17 @@
+import os
 from aiohttp import web
 from .stream_routes import routes
 
 def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
+
+    # -----------------[ STATIC FILES SETUP ]----------------- #
+    # This serves files from 'FileStream/static/' at the URL '/static/'
+    static_path = 'FileStream/static'
+    if os.path.isdir(static_path):
+        web_app.router.add_static('/static/', path=static_path, name='static')
+    else:
+        print(f"[WARNING] Static folder not found at: {static_path}. Local CSS/JS will not work.")
+        
     return web_app
