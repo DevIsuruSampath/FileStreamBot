8ed85357150aebfffee5d87423654de7cb2bce4f
Tulpa <openclaw@tulpa>
2026-01-30 12:03:11 +0000

    Fix: humanbytes handles zero/invalid sizes safely

--- FileStream/utils/human_readable.py
+++ FileStream/utils/human_readable.py
@@ -1,10 +1,16 @@
 def humanbytes(size):
-    if not size:
+    if size is None:
         return ""
+    try:
+        size = float(size)
+    except Exception:
+        return ""
+    if size <= 0:
+        return "0 B"
     power = 2**10
     n = 0
     Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
-    while size > power:
+    while size >= power and n < 4:
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
